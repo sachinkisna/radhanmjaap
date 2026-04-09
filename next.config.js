f9c3edb47f1a7417d0b8ad7314998747d06ebabb
Sachin Kumar Rout <sachinsoftware0001@gmail.com>
2026-04-09 11:45:05 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: true,
+};
+
+module.exports = nextConfig;
